0eb1932da618039ec20c3846ade4da27177e408e
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2017-07-18 17:16:32 +0200

    finished the worker's main

--- worker/main.go
+++ worker/main.go
@@ -4,6 +4,10 @@ import (
 	"fmt"
 	"image"
 	"os"
+	"strconv"
+	"sync"
+
+	"time"
 
 	"github.com/tsauvajon/go-microservices-poc/dataAccess"
 	"github.com/tsauvajon/go-microservices-poc/task"
@@ -36,6 +40,62 @@ func main() {
 		fmt.Println(err)
 		return
 	}
+
+	threadCount, err := strconv.Atoi(os.Args[2])
+
+	if err != nil {
+		fmt.Println("Error: ", "couldn't parse thread count")
+		return
+	}
+
+	waitGroup := sync.WaitGroup{}
+	waitGroup.Add(threadCount)
+
+	for i := 0; i < threadCount; i++ {
+		go func() {
+			for {
+				task, err := getNewTask(masterLocation)
+
+				if err != nil {
+					fmt.Println("Error: ", err)
+					fmt.Println("2s timeout")
+					time.Sleep(time.Second * 2)
+					continue
+				}
+
+				img, err := getImageFromStorage(storageLocation, task)
+
+				if err != nil {
+					fmt.Println("Error: ", err)
+					fmt.Println("2s timeout")
+					time.Sleep(time.Second * 2)
+					continue
+				}
+
+				img = doWorkOnImage(img)
+
+				err = sendImageToStorage(storageLocation, task, img)
+
+				if err != nil {
+					fmt.Println("Error: ", err)
+					fmt.Println("2s timeout")
+					time.Sleep(time.Second * 2)
+					continue
+				}
+
+				err = registerFinishedTask(masterLocation, task)
+
+				if err != nil {
+					fmt.Println("Error: ", err)
+					fmt.Println("2s timeout")
+					time.Sleep(time.Second * 2)
+					continue
+				}
+			}
+		}()
+	}
+
+	waitGroup.Wait()
 }
 
 func getNewTask(masterAddress string) (task.Task, error) {
