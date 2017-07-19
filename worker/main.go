2bf92633888e26db7b8fae449c9f4162bd64dc23
Thomas Sauvajon <thomas.sauvajon.dev@gmail.com>
2017-07-19 18:56:41 +0200

    implemented worker methods

--- worker/main.go
+++ worker/main.go
@@ -9,6 +9,18 @@ import (
 
 	"time"
 
+	"net/http"
+
+	"encoding/json"
+	"io/ioutil"
+
+	"image/color"
+	"image/png"
+
+	"bytes"
+
+	"errors"
+
 	"github.com/tsauvajon/go-microservices-poc/dataAccess"
 	"github.com/tsauvajon/go-microservices-poc/task"
 )
@@ -99,21 +111,111 @@ func main() {
 }
 
 func getNewTask(masterAddress string) (task.Task, error) {
-	return task.Task{}, nil
+	response, err := http.Post("http://"+masterAddress+"/getNewTask", "text/plain", nil)
+
+	if err != nil || response.StatusCode != http.StatusOK {
+		return task.Task{
+			ID:    -1,
+			State: -1,
+		}, err
+	}
+
+	data, err := ioutil.ReadAll(response.Body)
+
+	if err != nil {
+		return task.Task{
+			ID:    -1,
+			State: -1,
+		}, err
+	}
+
+	t := task.Task{}
+
+	err = json.Unmarshal(data, &t)
+
+	if err != nil {
+		return task.Task{
+			ID:    -1,
+			State: -1,
+		}, err
+	}
+
+	return t, nil
 }
 
 func getImageFromStorage(storageAddress string, t task.Task) (image.Image, error) {
-	return nil, nil
+	response, err := http.Get("http://" + storageAddress + "/getImage?state=working&id=" + strconv.Itoa(t.ID))
+
+	if err != nil || response.StatusCode != http.StatusOK {
+		return nil, err
+	}
+
+	// img, err := png.Decode(response.Body)
+
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// return img, nil
+
+	return png.Decode(response.Body)
 }
 
+// invert reds and greens
 func doWorkOnImage(img image.Image) image.Image {
-	return nil
+	canvas := image.NewRGBA(img.Bounds())
+
+	for i := 0; i < canvas.Rect.Max.X; i++ {
+		for j := 0; j < canvas.Rect.Max.Y; j++ {
+			r, g, b, a := img.At(i, j).RGBA()
+			color := new(color.RGBA)
+			color.R = uint8(g)
+			color.G = uint8(r)
+			color.B = uint8(b)
+			color.A = uint8(a)
+			canvas.Set(i, j, color)
+		}
+	}
+
+	return canvas.SubImage(img.Bounds())
 }
 
 func sendImageToStorage(storageAddress string, t task.Task, img image.Image) error {
+	data := []byte{}
+	buffer := bytes.NewBuffer(data)
+
+	err := png.Encode(buffer, img)
+
+	if err != nil {
+		return err
+	}
+
+	id := strconv.Itoa(t.ID)
+
+	response, err := http.Post("http://"+storageAddress+"/sendImage?state=finished&id="+id, "image/png", buffer)
+
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return errors.New("Error: " + "unexpected response => " + response.Status)
+	}
+
 	return nil
 }
 
 func registerFinishedTask(masterAddress string, t task.Task) error {
+	id := strconv.Itoa(t.ID)
+	response, err := http.Post("http://"+masterAddress+"/registerFinishedTask?id="+id, "text/plain", nil)
+
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return errors.New("Error: " + "unexpected response => " + response.Status)
+	}
+
 	return nil
 }
